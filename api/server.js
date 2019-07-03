79af705728ccf222d014e120d733800676f5b2ca
Edgar F <eflores.ef@gmail.com>
2019-07-02 21:48:04 -0700

    set up server

--- api/server.js
+++ api/server.js
@@ -1,5 +1,14 @@
 const express = require('express');
 
+const Videogame = require('./serverModel');
+
 const server = express();
 server.use(express.json());
 
+server.get('/', (req, res) => {
+
+})
+
+server.post('/', (req, res) => {
+   
+})
---
+++ api/serverModel.js
@@ -0,0 +1,14 @@
+const db = require('../data/dbConfig');
+
+module.exports = {
+   get,
+   insert
+}
+
+async function insert(videogame) {
+   return null;
+}
+
+function get() {
+   return null;
+}
